ed88d083c79c5bc59dff7174b0071f6938a5bc10
woodcrust <roboucrop@gmail.com>
2017-05-20 17:33:56 +0300

    add google maps key

--- mova/AppDelegate.swift
+++ mova/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        GMSServices.provideAPIKey("AIzaSyDS7wxxXS3KaWnZ_M8M-tzTJh-rTnC1CWA")
         
         window                     = UIWindow.init(frame: UIScreen.main.bounds)
         window?.backgroundColor    = UIColor.white
